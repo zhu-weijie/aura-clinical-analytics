640c0353e60204693b3522dbb635e2bd29a02dd3
zhuwj <byzhubaiyuan@gmail.com>
2025-08-08 12:55:37 +0800

    Update main.py to Run the Simulator

--- src/aura/main.py
+++ src/aura/main.py
@@ -1,5 +1,17 @@
+import time
+from aura.simulation.simulator import generate_clinical_data
+
+
 def run_app():
-    print("AURA: Automated Real-time Analytics clinical service started.")
+    print("AURA: Starting real-time clinical data simulation...")
+
+    try:
+        while True:
+            data_record = generate_clinical_data()
+            print(data_record)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\nSimulation stopped.")
 
 
 if __name__ == "__main__":
